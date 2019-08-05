84c38625506046a69daa61bab0147c46d12cbb34
Steve-2021 <stgio1@morgan.edu>
2019-08-05 09:49:19 -0400

    File

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -2,7 +2,31 @@ package hbcu.stay.ready.algor;
 
 public class Solution {
 
-    public String countUniqueWords(String input){
+    public String countUniqueWords(String input) {
+
+
+        String countUniqueWords = "that that is is that that is not is not is that it it is wutang";
+        String[] words = input.split(" ");
+        int wrc = 1;
+
+        for (int i = 0; i < words.length; i++)
+        {
+            for (int j = i + 1; j < words.length; j++)
+            {
+
+                if (words[i].equals(words[j]))
+                {
+                    wrc = wrc + 1;
+                    words[j] = "0";
+                }
+            }
+            if (words[i] != "0")
+                System.out.println(words[i] + "( Seen " + wrc );
+            wrc = 1;
+
+        }
+
+
         return null;
     }
 }
